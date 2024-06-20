a142f179500a88a6d8c4e87d26cbd209488e6c3b
jo33200 <jo33200@gmail.com>
2024-06-20 17:39:23 +0200

    refactor: Update Navbar link in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
     <header className="min-w-full bg-neutral-950 flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between h-32 md:h-16 fixed top-0 z-50 py-2">
       <nav className="min-w-full flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between sm:px-5 gap-5">
 
-        <a href='Home'><img src={Name} alt="mon nom" className='w-60 h-10' /></a>
+        <a href='#Home'><img src={Name} alt="mon nom" className='w-60 h-10' /></a>
         
         <ul className='text-gray-500 font-bold text-lg flex items-center justify-center space-x-4 sm:gap-5'>
           <li className='hover:text-white cursor-pointer'><a href='#Portfolio'>Portfolio</a></li>
